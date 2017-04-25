8cc62b8bf2732af7c21d3dc821616c9c42621db7
unknown <>
2017-04-25 14:38:27 +0900

    Project Created

---
+++ src/main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author puppy
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
